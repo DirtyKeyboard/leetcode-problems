3efde83d7513eb35f862094a8d2f4b3651992320
DirtyKeyboard <andrewxx000222@icloud.com>
2023-06-02 10:36:52 -0500

    125

---
+++ valid-palindrome-125/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    //remove : spaces and anything other than letters [non-alphanumeric]
+    
+    const clippedString = s.replace(/[^0-9a-z]/gi, '').toLowerCase()
+    let reverseString = ""
+    for (let i = clippedString.length-1; i >= 0; i--)
+    {
+        reverseString = reverseString.concat(clippedString.charAt(i))
+    }
+    if (reverseString === clippedString)
+        return true
+    else
+        return false
+};
